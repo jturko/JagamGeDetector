16813826a8b395a6d5389f290b2ddd42e8411725
jturko <JTurko@Joeys-MacBook-Pro-2.local>
2017-12-05 15:07:51 -0500

    change node (?)

--- JagamGeDetector.cc
+++ JagamGeDetector.cc
@@ -85,7 +85,7 @@ int main(int argc,char** argv)
       
   // Initialize G4 kernel
   //
-  runManager->Initialize();
+  //runManager->Initialize();
 
   // Get the pointer to the User Interface manager
   //
